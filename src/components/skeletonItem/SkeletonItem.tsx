ec5a8655eed76105c00db154f9a8ef4593150868
Alejandro-Vas <zended@yandex.ru>
2022-02-22 12:25:21 +0300

    SkeletinItem refactor

--- src/components/skeletonItem/SkeletonItem.tsx
+++ src/components/skeletonItem/SkeletonItem.tsx
@@ -1,19 +1,16 @@
 import { Skeleton } from "@mui/material";
+import { v4 as uuidv4 } from "uuid";
 
 const SkeletonItem = () => {
   return (
     <>
-      <Skeleton variant="text" />
-      <Skeleton variant="text" />
-      <Skeleton variant="text" />
-      <Skeleton variant="text" />
-      <Skeleton variant="text" />
-      <Skeleton variant="text" />
-      <Skeleton variant="text" />
+      {Array.from(Array(4).keys()).map((el) => {
+        return <Skeleton variant="text" key={uuidv4()} />;
+      })}
       <Skeleton variant="rectangular" width={350} height={250} />
-      <Skeleton variant="text" />
-      <Skeleton variant="text" />
-      <Skeleton variant="text" />
+      {Array.from(Array(20).keys()).map((el) => {
+        return <Skeleton variant="text" key={uuidv4()} />;
+      })}
     </>
   );
 };
