857c5a588c47112e5639d9bba1d63e6073459b8b
Aleksandr Vasin <zended@yandex.ru>
2023-02-19 15:43:31 +0300

    build: upd 18 react

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
 import './index.css';
 import { StyledEngineProvider } from '@mui/material/styles';
 import App from './components/app/App';
 
 import './styles/styles.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container)
+
+root.render(
+  <StrictMode>
     <StyledEngineProvider injectFirst>
       <App />
     </StyledEngineProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </StrictMode>,
 );
