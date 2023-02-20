e934b5cc61d090a9c5aa858badcad49c32263f1b
Aleksandr Vasin <zended@yandex.ru>
2023-02-20 12:00:08 +0300

    feat: chip

--- src/components/keywordsItem/KeywordsItem.tsx
+++ src/components/keywordsItem/KeywordsItem.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material';
+import { Chip } from '@mui/material';
 import useActions from '../../hooks/useActions';
 
 interface IProps {
@@ -7,23 +7,20 @@ interface IProps {
 
 function KeywordsItem({ keyword }:IProps) {
   const { setArticleSearchQuery } = useActions();
+
   const handleClick = () => {
     setTimeout(() => setArticleSearchQuery(keyword), 500);
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
+  const text = keyword.length < 30 ? keyword : `${keyword.slice(0, 30)}...`
+
   return (
-    <>
-      <Button
-        className="keywords-button"
-        variant="contained"
-        onClick={handleClick}
-        sx={{ fontSize: 12 }}
-      >
-        {keyword.length < 30 ? keyword : `${keyword.slice(0, 30)}...`}
-      </Button>
-      {' '}
-    </>
+    <Chip
+      onClick={handleClick}
+      color="primary"
+      label={text}
+    />
   );
 }
 export default KeywordsItem;
