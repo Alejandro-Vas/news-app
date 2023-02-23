f4b1b36986100921f5c1b70406ae94bad118fc7d
Aleksandr Vasin <zended@yandex.ru>
2023-02-23 19:28:45 +0300

    refactor: arrBar styles

---
+++ src/components/AppBar/styles.ts
@@ -0,0 +1,57 @@
+const styles = {
+  appBar: {
+    display: 'flex',
+  },
+
+  drawerWrapper: {
+    display: {
+      xs: 'block',
+      sm: 'none',
+    },
+    '& .MuiDrawer-paper': {
+      boxSizing: 'border-box',
+      width: 240,
+    },
+  },
+
+  drawer: {
+    textAlign: 'center',
+  },
+
+  listItemButton: {
+    textAlign: 'center',
+  },
+
+  listItemText: {
+    color: 'text.primary',
+  },
+
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+
+  iconButton: {
+    mr: 2,
+    display: { sm: 'none' },
+  },
+
+  h1: {
+    flexGrow: 1,
+    color: 'background.paper',
+    p: 1,
+  },
+
+  navItemsWrapper: {
+    display: {
+      xs: 'none',
+      sm: 'block',
+    },
+  },
+
+  navLink: {
+    color: 'background.paper',
+  },
+};
+
+export default styles;
