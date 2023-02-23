6072b14843f4fbfb6df0f21689f927ba43d828c4
Aleksandr Vasin <zended@yandex.ru>
2023-02-23 19:17:34 +0300

    fix: theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
-import './index.css';
 import App from './components/app/App';
 
 const container = document.getElementById('root') as HTMLElement;
