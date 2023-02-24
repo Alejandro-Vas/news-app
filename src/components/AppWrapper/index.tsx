589ac8658961d662fa1d8bcb6a2ae899642c896e
Aleksandr Vasin <zended@yandex.ru>
2023-02-24 12:31:28 +0300

    fix: wrapper closeIcon

--- src/components/AppWrapper/index.tsx
+++ src/components/AppWrapper/index.tsx
@@ -1,18 +1,33 @@
 import { store } from 'store/store';
 import { Provider } from 'react-redux';
-import { SnackbarProvider } from 'notistack';
+import { SnackbarProvider, useSnackbar } from 'notistack';
 
 import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 import App from 'components/App/index';
 
 import theme from 'theme/index';
 
+import { IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+
+function SnackbarCloseButton({ snackbarKey }:{snackbarKey: string | number}) {
+  const { closeSnackbar } = useSnackbar();
+
+  return (
+    <IconButton onClick={() => closeSnackbar(snackbarKey)}>
+      <CloseIcon sx={{ color: 'background.paper' }} />
+    </IconButton>
+  );
+}
+
 function AppWrapper() {
   return (
     <Provider store={store}>
       <StyledEngineProvider injectFirst>
         <ThemeProvider theme={theme}>
-          <SnackbarProvider>
+          <SnackbarProvider
+            action={(snackbarKey) => <SnackbarCloseButton snackbarKey={snackbarKey} />}
+          >
             <App />
           </SnackbarProvider>
         </ThemeProvider>
