a75c8bd932265684473b20327f569238c31657b9
Aleksandr Vasin <zended@yandex.ru>
2023-02-25 13:58:51 +0300

    feat: favorite LS

---
+++ src/store/middlewares/localStorageMiddlware.ts
@@ -0,0 +1,14 @@
+import { Middleware } from '@reduxjs/toolkit';
+
+export const localStorageMiddleware:Middleware = ({ getState }) => (next) => (action) => {
+  const result = next(action);
+  localStorage.setItem('applicationState', JSON.stringify(getState()));
+  return result;
+};
+
+export const reHydrateStore = () => {
+  if (localStorage.getItem('applicationState') !== null) {
+    return JSON.parse(localStorage.getItem('applicationState') || '');
+  }
+  return undefined;
+};
--- src/store/store.ts
+++ src/store/store.ts
@@ -4,6 +4,7 @@ import articleSearchQuery from './slices/articleSearchQuerySlice';
 import favorite from './slices/favorite';
 import notifications from './slices/notifications';
 import rtkQueryErrorLogger from './middlewares/rtkQueryErrorLogger';
+import { reHydrateStore, localStorageMiddleware } from './middlewares/localStorageMiddlware';
 
 export const store = configureStore({
   reducer: {
@@ -12,9 +13,11 @@ export const store = configureStore({
     favorite: favorite.reducer,
     notifications: notifications.reducer,
   },
+  preloadedState: reHydrateStore(),
   middleware: (getDefaultMiddleware) => getDefaultMiddleware()
     .concat(articleSearchApi.middleware)
-    .concat(rtkQueryErrorLogger),
+    .concat(rtkQueryErrorLogger)
+    .concat(localStorageMiddleware),
 });
 
 export type TypeRootState = ReturnType<typeof store.getState>;
