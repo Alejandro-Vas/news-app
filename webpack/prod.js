eb7cee3d40001326af85309aee992a5e8d08d002
Aleksandr Vasin <zended@yandex.ru>
2023-02-27 12:04:31 +0300

    fix: prod build

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,10 +1,12 @@
+/* eslint-disable import/extensions */
 /* eslint-disable import/no-extraneous-dependencies */
 import { resolve, join } from 'path';
 import { merge } from 'webpack-merge';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import common from './common';
+import common from './common.js';
 
-const DIR = resolve(__dirname, '../..');
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = resolve();
 
 export default merge(common, {
   mode: 'production',
@@ -13,7 +15,7 @@ export default merge(common, {
   },
 
   output: {
-    path: join(DIR, 'build'),
+    path: join(__dirname, 'build'),
     publicPath: '/',
     filename: (src) => `${src.runtime}.${src.chunk.contentHash.javascript}.js`,
     sourceMapFilename: '[file].map',
@@ -21,7 +23,7 @@ export default merge(common, {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: join(DIR, 'src/index.html'),
+      template: join(__dirname, 'src/index.html'),
       minify: true,
     }),
   ],
